b0b169f25d470c547fe294004e117b8e2f3a89ab
Aman3213 <amanuelliben3000@gmail.com>
2022-10-21 15:17:49 +0300

    AppointmentDate modified

--- AdminPage/AppointmentForm.designer.cs
+++ AdminPage/AppointmentForm.designer.cs
@@ -74,9 +74,10 @@
             this.bunifuPanel1.Controls.Add(this.label1);
             this.bunifuPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.bunifuPanel1.Location = new System.Drawing.Point(0, 0);
+            this.bunifuPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bunifuPanel1.Name = "bunifuPanel1";
             this.bunifuPanel1.ShowBorders = true;
-            this.bunifuPanel1.Size = new System.Drawing.Size(323, 512);
+            this.bunifuPanel1.Size = new System.Drawing.Size(431, 675);
             this.bunifuPanel1.TabIndex = 0;
             // 
             // label6
@@ -84,17 +85,20 @@
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(32, 376);
+            this.label6.Location = new System.Drawing.Point(43, 465);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(152, 21);
+            this.label6.Size = new System.Drawing.Size(188, 27);
             this.label6.TabIndex = 13;
             this.label6.Text = "Appointment Date";
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(36, 400);
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(48, 513);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(245, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(325, 30);
             this.dateTimePicker1.TabIndex = 12;
             // 
             // bunifuButton2
@@ -144,7 +148,8 @@
             this.bunifuButton2.IdleIconLeftImage = null;
             this.bunifuButton2.IdleIconRightImage = null;
             this.bunifuButton2.IndicateFocus = false;
-            this.bunifuButton2.Location = new System.Drawing.Point(172, 456);
+            this.bunifuButton2.Location = new System.Drawing.Point(224, 592);
+            this.bunifuButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bunifuButton2.Name = "bunifuButton2";
             this.bunifuButton2.OnDisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
             this.bunifuButton2.OnDisabledState.BorderRadius = 1;
@@ -178,7 +183,7 @@
             this.bunifuButton2.OnPressedState.ForeColor = System.Drawing.Color.White;
             this.bunifuButton2.OnPressedState.IconLeftImage = null;
             this.bunifuButton2.OnPressedState.IconRightImage = null;
-            this.bunifuButton2.Size = new System.Drawing.Size(64, 35);
+            this.bunifuButton2.Size = new System.Drawing.Size(85, 43);
             this.bunifuButton2.TabIndex = 11;
             this.bunifuButton2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.bunifuButton2.TextAlignment = System.Windows.Forms.HorizontalAlignment.Center;
@@ -234,7 +239,8 @@
             this.bunifuButton1.IdleIconLeftImage = null;
             this.bunifuButton1.IdleIconRightImage = null;
             this.bunifuButton1.IndicateFocus = false;
-            this.bunifuButton1.Location = new System.Drawing.Point(75, 456);
+            this.bunifuButton1.Location = new System.Drawing.Point(99, 592);
+            this.bunifuButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bunifuButton1.Name = "bunifuButton1";
             this.bunifuButton1.OnDisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
             this.bunifuButton1.OnDisabledState.BorderRadius = 1;
@@ -268,7 +274,7 @@
             this.bunifuButton1.OnPressedState.ForeColor = System.Drawing.Color.White;
             this.bunifuButton1.OnPressedState.IconLeftImage = null;
             this.bunifuButton1.OnPressedState.IconRightImage = null;
-            this.bunifuButton1.Size = new System.Drawing.Size(64, 35);
+            this.bunifuButton1.Size = new System.Drawing.Size(85, 43);
             this.bunifuButton1.TabIndex = 10;
             this.bunifuButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.bunifuButton1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Center;
@@ -281,9 +287,10 @@
             // 
             this.cmb_room.Font = new System.Drawing.Font("Segoe UI Variable Text Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmb_room.FormattingEnabled = true;
-            this.cmb_room.Location = new System.Drawing.Point(36, 330);
+            this.cmb_room.Location = new System.Drawing.Point(48, 406);
+            this.cmb_room.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmb_room.Name = "cmb_room";
-            this.cmb_room.Size = new System.Drawing.Size(245, 29);
+            this.cmb_room.Size = new System.Drawing.Size(325, 35);
             this.cmb_room.TabIndex = 9;
             // 
             // label5
@@ -291,9 +298,10 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(36, 306);
+            this.label5.Location = new System.Drawing.Point(48, 377);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(55, 21);
+            this.label5.Size = new System.Drawing.Size(70, 27);
             this.label5.TabIndex = 8;
             this.label5.Text = "Room";
             // 
@@ -301,9 +309,10 @@
             // 
             this.cmb_nurse.Font = new System.Drawing.Font("Segoe UI Variable Text Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmb_nurse.FormattingEnabled = true;
-            this.cmb_nurse.Location = new System.Drawing.Point(36, 258);
+            this.cmb_nurse.Location = new System.Drawing.Point(48, 318);
+            this.cmb_nurse.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmb_nurse.Name = "cmb_nurse";
-            this.cmb_nurse.Size = new System.Drawing.Size(245, 29);
+            this.cmb_nurse.Size = new System.Drawing.Size(325, 35);
             this.cmb_nurse.TabIndex = 7;
             // 
             // label4
@@ -311,9 +320,10 @@
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(36, 234);
+            this.label4.Location = new System.Drawing.Point(48, 288);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 21);
+            this.label4.Size = new System.Drawing.Size(68, 27);
             this.label4.TabIndex = 6;
             this.label4.Text = "Nurse";
             // 
@@ -321,9 +331,10 @@
             // 
             this.cmb_doctor.Font = new System.Drawing.Font("Segoe UI Variable Text Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmb_doctor.FormattingEnabled = true;
-            this.cmb_doctor.Location = new System.Drawing.Point(36, 191);
+            this.cmb_doctor.Location = new System.Drawing.Point(48, 235);
+            this.cmb_doctor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmb_doctor.Name = "cmb_doctor";
-            this.cmb_doctor.Size = new System.Drawing.Size(245, 29);
+            this.cmb_doctor.Size = new System.Drawing.Size(325, 35);
             this.cmb_doctor.TabIndex = 5;
             // 
             // label3
@@ -331,9 +342,10 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(32, 167);
+            this.label3.Location = new System.Drawing.Point(43, 206);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 21);
+            this.label3.Size = new System.Drawing.Size(78, 27);
             this.label3.TabIndex = 4;
             this.label3.Text = "Doctor";
             // 
@@ -341,9 +353,10 @@
             // 
             this.cmb_patient.Font = new System.Drawing.Font("Segoe UI Variable Text Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmb_patient.FormattingEnabled = true;
-            this.cmb_patient.Location = new System.Drawing.Point(36, 121);
+            this.cmb_patient.Location = new System.Drawing.Point(48, 149);
+            this.cmb_patient.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmb_patient.Name = "cmb_patient";
-            this.cmb_patient.Size = new System.Drawing.Size(245, 29);
+            this.cmb_patient.Size = new System.Drawing.Size(325, 35);
             this.cmb_patient.TabIndex = 3;
             // 
             // label2
@@ -351,9 +364,10 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(36, 97);
+            this.label2.Location = new System.Drawing.Point(48, 119);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 21);
+            this.label2.Size = new System.Drawing.Size(77, 27);
             this.label2.TabIndex = 2;
             this.label2.Text = "Patient";
             // 
@@ -362,9 +376,10 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(70, 26);
+            this.label1.Location = new System.Drawing.Point(93, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(194, 26);
+            this.label1.Size = new System.Drawing.Size(246, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Book Appointment";
             // 
@@ -374,10 +389,11 @@
             // 
             // AppointmentForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(323, 512);
+            this.ClientSize = new System.Drawing.Size(431, 675);
             this.Controls.Add(this.bunifuPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AppointmentForm";
             this.Text = "AppointmentForm";
             this.Load += new System.EventHandler(this.AppointmentForm_Load);
